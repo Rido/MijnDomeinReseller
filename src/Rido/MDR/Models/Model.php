d94484cfe7f151e44914949421e521090fdf5de8
Rick Doorakkers <rickdoorakkers@gmail.com>
2016-09-30 21:07:45 +0200

    * Improved Model

--- src/Rido/MDR/Models/Model.php
+++ src/Rido/MDR/Models/Model.php
@@ -12,12 +12,44 @@ abstract class Model
     /**
      * @var array
      */
-    private $attributes;
+    protected $attributes = [];
 
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * Model constructor.
+     *
+     * @param Connection $connection
+     * @param array      $attributes
+     */
     public function __construct(Connection $connection, array $attributes = [])
     {
         $this->connection = $connection;
-        $this->attributes = $attributes;
+        $this->fill($attributes);
+    }
+
+    /**
+     * @param array $attributes
+     */
+    protected function fill(array $attributes = [])
+    {
+        foreach ($attributes as $key => $value) {
+            if ($this->isFillable($key)) {
+                $this->attributes[$key] = $value;
+            }
+        }
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    protected function isFillable($key)
+    {
+        return in_array($key, $this->fillable);
     }
 
     /**
@@ -34,5 +66,40 @@ abstract class Model
     public function getAttributes()
     {
         return $this->attributes;
+    }
+
+    /**
+     * @param $result
+     * @return mixed
+     */
+    public function createObjectFromResponse($result)
+    {
+        $modelName = get_class($this);
+
+        $model = new $modelName($this->connection, $result);
+
+        return $model;
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        if (isset($this->attributes[$key])) {
+            return $this->attributes[$key];
+        }
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function __set($key, $value)
+    {
+        if ($this->isFillable($key)) {
+            $this->attributes[$key] = $value;
+        }
     }
 }
