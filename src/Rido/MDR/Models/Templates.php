bd51a86e615799ccb91fccfc2c07552cca2176fd
F. Kraijenoord <32488174+bedrijfsportaal@users.noreply.github.com>
2018-03-11 13:53:59 +0100

    * New commands added
    Commands available on templates management.

--- src/Rido/MDR/Models/Templates.php
+++ src/Rido/MDR/Models/Templates.php
@@ -2,26 +2,81 @@
 
 namespace Rido\MDR\Models;
 
-class Templates extends Model
+class Templates extends Model 
 {
-    /**
-     * @var array
-     */
-    protected $fillable = [
-        'template_id',
-        'template_name',
-        'template_connections',
-    ];
+	/**
+	 * @var array
+	 */
+	protected $fillable = [
+		'template_id',
+		'template_name'
+	];
+
+	/**
+	 * @return bool
+	 */
+	public function get() 
+	{
+		if ($result = $this->connection->get('dns_template_list')) {
+			return $result['items'];
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function find($id)
+	{
+		$result = $this->connection->get('dns_template_get_details', [
+			"template_id" => $id,
+		]);
+
+		return $result;
+	}
 
     /**
-     * @return bool
+     * @param array $attributes
+     *
+     * @return array
      */
-    public function get()
+    public function create(array $attributes = [])
     {
-        if ($result = $this->connection->get('dns_get_templates')) {
-            return $result['items'];
-        }
+        $this->fill($attributes);
 
-        return false;
+        $result = $this->connection->put('dns_template_record_add', $this->attributes);
+
+        return $result;
+    }
+
+    /**
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function remove(array $attributes = [])
+    {
+        $this->fill($attributes);
+
+        $result = $this->connection->put('dns_template_record_del', $this->attributes);
+
+        return $result;
+    }
+
+    /**
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function ttl(array $attributes = [])
+    {
+        $this->fill($attributes);
+
+        $result = $this->connection->put('dns_template_ttl_modify', $this->attributes);
+
+        return $result;
     }
 }
